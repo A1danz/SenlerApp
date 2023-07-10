3961bc97b49b966707e5d5bc2d5bec43f3038ec1
A1danz <galeevaidan@yandex.ru>
2023-07-10 17:24:24 +0300

    create post states in db

--- app/src/main/java/com/itis/senlerapp/db/Posts.kt
+++ app/src/main/java/com/itis/senlerapp/db/Posts.kt
@@ -7,7 +7,10 @@ object Posts : BaseColumns {
 
     const val COLUMN_NAME_TEXT = "text"
     const val COLUMN_NAME_DATE = "date"
-    const val COLUMN_NAME_PHOTOS = "photos"
+    const val COLUMN_NAME_VK = "vk"
+    const val COLUMN_NAME_VK_GROUP = "vk_group"
+    const val COLUMN_NAME_TG = "tg"
+    const val COLUMN_NAME_INST = "inst"
 
     const val DATABASE_VERSION = 1
     const val DATABASE_NAME = "SenlerDb.db"
@@ -16,7 +19,11 @@ object Posts : BaseColumns {
         "CREATE TABLE IF NOT EXISTS $TABLE_NAME (" +
                 "${BaseColumns._ID} INTEGER PRIMARY KEY," +
                 "$COLUMN_NAME_TEXT TEXT," +
-                "$COLUMN_NAME_DATE BIGINT)"
+                "$COLUMN_NAME_DATE BIGINT," +
+                "$COLUMN_NAME_VK BOOLEAN," +
+                "$COLUMN_NAME_VK_GROUP BOOLEAN," +
+                "$COLUMN_NAME_TG BOOLEAN," +
+                "$COLUMN_NAME_INST BOOLEAN)"
 
     const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS $TABLE_NAME"
 
